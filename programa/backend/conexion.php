d67900550ae8ae32964d8f28bc4b0ff12c592f72
proyectojessica <proyectojessica83@gmail.com>
2026-05-29 09:16:54 -0600

    Create conexion.php

---
+++ programa/backend/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$usuario = 'p24330050470371';
+$contrasena = 'cra6371&LO';
+$basedatos = 'bd24330050470371';
+
+$conn = new mysqli($host, $usuario, $contrasena, $basedatos);
+
+if ($conn->connect_error) {
+    die("CONEXIÃ“N FALLIDA: " . $conn->connect_error);
+}
+
+?>
